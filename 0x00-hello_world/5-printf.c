00ca42677beb0dff8cca08eb989ee99152a61fb0
ighodarodaniel1995 <ighodarod95@gmail.com>
2022-03-11 03:52:35 +0100

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-*Return: Always 0 (success)
-*/
+ * main - Entry point
+ * 
+ * Return: Always 0 (success)
+ */
 int main (void)
 {
-printf("with the proper grammer, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0);
 }
